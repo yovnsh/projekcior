13fa1ecfe3da129d510db1020658ae90c7dd183a
yovnsh <yovnsh@gmail.com>
2022-12-10 23:37:15 +0100

    yes yes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Projekcior {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
